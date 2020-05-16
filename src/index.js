afa76df9ced6123cc89eac3c6938ba746e9bf73f
Etienne Cadillon <djctavia@gmail.com>
2020-05-16 06:33:13 +0200

    Project started

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            window: {
+                width: 1920,
+                height: 1080,
+            },
+        };
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener("resize", this.updateWindowDimensions.bind(this));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateWindowDimensions.bind(this));
+    }
+
+    updateWindowDimensions() {
+        this.setState({window: {width: window.innerWidth, height: window.innerHeight}});
+    }
+
+    generateGrid(gridSize) {
+        const gridList = [];
+
+        for (let iGrid = 0; iGrid < gridSize * gridSize; ++iGrid) {
+            let digit = iGrid % 10;
+            let cssClass;
+
+            if ((iGrid  - digit) % 20) {
+                cssClass = iGrid % 2 ? "container red" : "container blue";
+            } else {
+                cssClass = iGrid % 2 ? "container blue" : "container red";
+            }
+            gridList.push(
+                <div className={cssClass}><h1>{iGrid}</h1></div>
+            );
+        }
+        return gridList;
+    }
+
+    render() {
+        return (
+            <Router>
+                <aside>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/servers">Servers</Link>
+                        </li>
+                        <li>
+                            <Link to="/game">Game</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                    </ul>
+                </aside>
+                <Switch>
+                    <h1>{this.state.window.width} x {this.state.window.height}</h1>
+                    <div className="grid" style={{height: this.state.window.height, width: this.state.window.height}}>
+                        {this.generateGrid(10)}
+                    </div>
+                </div>
+                </Switch>
+            </Router>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
